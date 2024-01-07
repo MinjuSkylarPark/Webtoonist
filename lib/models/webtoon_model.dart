c0fcb490fea78ab594d1c42be3f8a5db9e0b92cc
SkylarPark <80894789+MinjuSkylarPark@users.noreply.github.com>
2024-01-07 17:09:20 +0900

    paused the webtoon app will be back soon

--- lib/models/webtoon_model.dart
+++ lib/models/webtoon_model.dart
@@ -3,3 +3,6 @@ class Webtoonmodel {
 
   Webtoonmodel({required this.title, required this.thumb, required this.id});
 }
+
+//잠깐 중지 넥스프 프로젝트완료후 다시 플러터로 되돌아올 예정
+//당장 월요일에 자바스크립트 프로젝트가 있는데 그거 리캡 끝내고 웹툰앱으로 복귀할것임
